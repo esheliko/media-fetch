d2dbc995fc9371a4dbec70fd22658a3e4f56c6ca
Esheliko <eshel@smartprinter.co.il>
2022-09-25 14:25:06 +0300

    Set react-query defaults to not refetch on window focus

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
